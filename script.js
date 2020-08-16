8632fe97e7f2d9b6414e167a4a6f0d9799bbc2e8
Hussein Salat <husseinsalat24@gmail.com>
2020-08-16 17:39:20 +0300

    looping in js

--- script.js
+++ script.js
@@ -2,12 +2,24 @@ for (var i = 0; i < 3; i++) {
   Start(i);
 }
 
-function start(i){
-    var percent=document.querySelector(".progress${i} .percent-num");
-    var progressing=document.querySelector(".progress${i} .progressing");
-    var margin=document.querySelector(".progress${i} .margin");
+function start(i) {
+  var percent = document.querySelector(".progress${i} .percent-num");
+  var progressing = document.querySelector(".progress${i} .progressing");
+  var margin = document.querySelector(".progress${i} .margin");
 
-    requestAnimationFrame(grow);
+  requestAnimationFrame(grow);
 
-    var progress = 0;
+  var progress = 0;
+  var random = parseInt(Math.random() * 100);
+
+  function grow() {
+    progress += 1;
+    console.log(random);
+    if (progress <= random) {
+      percent.innerHTML = parseInt(progress) + "%";
+      progressing.getElementsByClassName.width = progress + "%";
+      margin.getElementsByClassName.left=progress+"%";
+      requestAnimationFrame(grow)
+    }
+  }
 }
